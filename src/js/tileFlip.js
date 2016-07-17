72e6d0241ca92db5ba48a35e5bea47309ff432e3
Chris Asakawa <chris@asakawa.me>
2016-07-17 15:01:28 -0700

    created spinning effect for tiles

---
+++ src/js/tileFlip.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Christopher on 7/17/2016.
+ */
+$(document).ready(function(){
+    // set up hover panels
+    // although this can be done without JavaScript, we've attached these events
+    // because it causes the hover to be triggered when the element is tapped on a touch device
+    $('.hover').hover(function(){
+        $(this).addClass('flip');
+    },function(){
+        $(this).removeClass('flip');
+    });
+});
